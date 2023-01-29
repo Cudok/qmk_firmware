87831378284a412b2ef4a1e51950054d12537fab
dokcu <falk@pop-os.localdomain>
2023-01-29 11:04:40 +0100

    small comment

--- keyboards/splitkb/kyria/keymaps/dokcu/rules.mk
+++ keyboards/splitkb/kyria/keymaps/dokcu/rules.mk
@@ -1,5 +1,5 @@
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGB_MATRIX_ENABLE = no     # This Option I use also for my crkbd and sofle choc Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
