e61f0145ccd56b0fe130b3ac15d8d230e10d0e40
dokcu <falk@pop-os.localdomain>
2023-02-18 16:33:56 +0100

    try to use rgb_matrix

---
+++ keyboards/splitkb/kyria/keymaps/dokcu2/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306      # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = yes     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+RGB_MATRIX_DRIVER = WS2812
+## WS2812_DRIVER = bitbang
