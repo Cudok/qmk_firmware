0d661c988888ed31b1d3686eb7d81c48bb35123f
dokcu <falk@pop-os.localdomain>
2023-03-03 18:02:49 +0100

    tab dance dokcu

--- keyboards/splitkb/kyria/keymaps/dokcu/keymap.c
+++ keyboards/splitkb/kyria/keymaps/dokcu/keymap.c
@@ -25,6 +25,17 @@ enum layers {
     _ADJUST,
 };
 
+// Tap Dance declarations
+enum {
+    TD_SHIFT_CAPS,
+};
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+};
+
 
 // Aliases for readability
 #define QWERTY   DF(_QWERTY)
@@ -64,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      QK_GESC , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      KC_TAB  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , ADJUST ,KC_MUTE,     FKEYS  , KC_SPC , KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+     TD(TD_SHIFT_CAPS), KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , ADJUST ,KC_MUTE,     FKEYS  , KC_SPC , KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 KC_LGUI, KC_LCTL, KC_LALT, KC_SPC , SYM   ,     NAV    ,KC_ENTER,KC_RALT, KC_RCTL, KC_APP
     ),
 
--- keyboards/splitkb/kyria/keymaps/dokcu/rules.mk
+++ keyboards/splitkb/kyria/keymaps/dokcu/rules.mk
@@ -3,3 +3,5 @@ OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # This Option I use also for my crkbd and sofle choc Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+TAP_DANCE_ENABLE=yes	   # Enables Tap Dance
