b8dbe8c5dd9ff15e4c0f15df6969b1105c706a51
dokcu <falk@pop-os.localdomain>
2023-03-08 16:25:56 +0100

    rename tab dance left shift caps look

--- keyboards/crkbd/keymaps/17_dokcu_crkbd/keymap.c
+++ keyboards/crkbd/keymaps/17_dokcu_crkbd/keymap.c
@@ -25,15 +25,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _ADJUST 3
 // Tap Dance declarations
 enum {
-    TD_SHIFT_CAPS,
+    TD_SHIFT_CAPS_L,
 };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
-    [TD_SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    [TD_SHIFT_CAPS_L] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
 };
 
+#define TD_LSFT TD(TD_SHIFT_CAPS_L)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // base layer
@@ -43,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-TD(TD_SHIFT_CAPS), KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   MO(2),
+      TD_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   MO(2),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL, KC_LALT,  KC_SPC,   KC_ENT, MO(1), KC_RALT
                                       //`--------------------------'  `--------------------------'
